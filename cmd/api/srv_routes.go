943571e10e7fcb85c66cf1168d72e5307465c22c
Niewolinsky <niewolinski@protonmail.com>
2023-02-13 12:02:09 +0100

    Integrated Swagger auto-documentation in its basic form

--- cmd/api/srv_routes.go
+++ cmd/api/srv_routes.go
@@ -3,15 +3,8 @@ package main
 import (
 	"net/http"
 
-	_ "viadro_api/docs"
-
 	"github.com/julienschmidt/httprouter"
-	httpSwagger "github.com/swaggo/http-swagger"
 )
-
-func (app *application) documentationHandler(res http.ResponseWriter, req *http.Request, p httprouter.Params) {
-	httpSwagger.WrapHandler(res, req)
-}
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
